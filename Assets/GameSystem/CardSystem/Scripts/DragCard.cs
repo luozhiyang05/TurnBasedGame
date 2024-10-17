5a1125f78f7b39ddfcf2b6725c1279f55a34e832
lzy <940783983@qq.com>
2024-10-18 00:03:51 +0800

    feat：新增UI的absUnit代替世界absUnit

--- Assets/GameSystem/CardSystem/Scripts/DragCard.cs
+++ Assets/GameSystem/CardSystem/Scripts/DragCard.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Framework;
 using GameSystem.BattleSystem;
 using GameSystem.BattleSystem.Scripts;
@@ -29,43 +30,52 @@ namespace GameSystem.CardSystem.Scripts
                 Vector2 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
                 RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
 
-                if (hit.collider != null)
+                PointerEventData pointerEvent = new(EventSystem.current);
+                pointerEvent.position = Input.mousePosition;
+                List<RaycastResult> raycastResults = new List<RaycastResult>();
+                EventSystem.current.RaycastAll(pointerEvent,raycastResults);
+                if (raycastResults.Count>0)
                 {
-                    //获取鼠标光标下的Unit
-                    GameObject currentObjectUnderCursor = hit.collider.gameObject;
+                    Debug.Log(raycastResults[0].gameObject.name);
+                }
 
-                    //判断当前行动带你是否足够使用卡牌
-                    var player = this.GetSystem<IBattleSystemModule>().GetPlayerUnit() as Player;
-                    if (player.nowActPoint >= BaseCardSo.depletePoint)
-                    {
-                        //使用卡牌的命令
-                        this.SendCmd<UseCardCmd, CardData>(new CardData()
-                        {
-                            user = player,
-                            headCardIdx = headCardIdx,
-                            cardSo = BaseCardSo,
-                            target = currentObjectUnderCursor
-                        });
-                        return;
-                    }
-                }
-                else
-                {
-                    //当拖拽距离大于350且当前卡牌可以自动使用，则自动使用
-                    var player = this.GetSystem<IBattleSystemModule>().GetPlayerUnit() as Player;
-                    if (BaseCardSo.canAutoUse && GetDargDistance() > 350f)
-                    {
-                        //使用卡牌的命令
-                        this.SendCmd<UseCardCmd, CardData>(new CardData()
-                        {
-                            user = player,
-                            headCardIdx = headCardIdx,
-                            cardSo = BaseCardSo,
-                            target = player.gameObject  //自动使用的卡牌目标为自己
-                        });
-                        return;
-                    }
-                }
+                // if (hit.collider != null)
+                // {
+                //     //获取鼠标光标下的Unit
+                //     GameObject currentObjectUnderCursor = hit.collider.gameObject;
+
+                //     //判断当前行动带你是否足够使用卡牌
+                //     var player = this.GetSystem<IBattleSystemModule>().GetPlayerUnit() as Player;
+                //     if (player.nowActPoint >= BaseCardSo.depletePoint)
+                //     {
+                //         //使用卡牌的命令
+                //         this.SendCmd<UseCardCmd, CardData>(new CardData()
+                //         {
+                //             user = player,
+                //             headCardIdx = headCardIdx,
+                //             cardSo = BaseCardSo,
+                //             target = currentObjectUnderCursor
+                //         });
+                //         return;
+                //     }
+                // }
+                // else
+                // {
+                //     //当拖拽距离大于350且当前卡牌可以自动使用，则自动使用
+                //     var player = this.GetSystem<IBattleSystemModule>().GetPlayerUnit() as Player;
+                //     if (BaseCardSo.canAutoUse && GetDargDistance() > 350f)
+                //     {
+                //         //使用卡牌的命令
+                //         this.SendCmd<UseCardCmd, CardData>(new CardData()
+                //         {
+                //             user = player,
+                //             headCardIdx = headCardIdx,
+                //             cardSo = BaseCardSo,
+                //             target = player.gameObject  //自动使用的卡牌目标为自己
+                //         });
+                //         return;
+                //     }
+                // }
             }
 
             SetCanBlockRaycasts(true);
