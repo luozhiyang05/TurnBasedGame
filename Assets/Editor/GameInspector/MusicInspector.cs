9c6dfe9a862f902bf582b412b32eade641c77afa
LAPTOP-GPM9596R\luozhiyang <940783983@qq.com>
2025-08-04 20:32:06 +0800

    音符生成工具1.1 新增空格暂停播放 新增生成连续音符

--- Assets/Editor/GameInspector/MusicInspector.cs
+++ Assets/Editor/GameInspector/MusicInspector.cs
@@ -12,6 +12,9 @@ namespace GameInspector
         private AudioSource _previewAudioSource;
         private float _nowMusicTime;
         private bool _isStartUpdate;
+        private float _intervalNoteTime;
+        private int _createIntervalNoteCnt;
+        private bool _createNoteUpType = true;
 
         void OnEnable()
         {
@@ -40,24 +43,13 @@ namespace GameInspector
 
         public override void OnInspectorGUI()
         {
-            //在 EditorApplication.update ，Event会报空。这里用于快速添加音符
-            if (Event.current.type == EventType.KeyDown && _isStartUpdate && _nowMusicTime >= _target.reachToBitPosTime)
-            {
-                if (Event.current.keyCode == KeyCode.J || Event.current.keyCode == KeyCode.K)
-                {
-                    _target.AddNote(_nowMusicTime, ENotePos.Up, ENoteType.Click);
-                }
-                else if (Event.current.keyCode == KeyCode.D || Event.current.keyCode == KeyCode.F)
-                {
-                    _target.AddNote(_nowMusicTime, ENotePos.Down, ENoteType.Click);
-                }
-                Event.current.Use();  // 阻止事件冒泡
-            }
+            //处理输入
+            HandleKeyDown();
 
             //容错
             if (null == _target.audioClip) EditorGUILayout.HelpBox("请先设置预览音频源！", MessageType.Error);
 
-            //按钮
+            //播放/暂停 按钮
             if (GUILayout.Button(!_isStartUpdate ? "播放" : "暂停"))
             {
                 if (null == _target.audioClip)
@@ -65,41 +57,97 @@ namespace GameInspector
                     Debug.LogError("请先设置预览音频源！");
                     return;
                 }
-
                 _isStartUpdate = !_isStartUpdate;
-                if (_isStartUpdate)
-                {
-                    _previewAudioSource.clip = _target.audioClip;
-                    _previewAudioSource.time = _nowMusicTime;
-                    _previewAudioSource.pitch = _target.musicSpeed;
-                    _previewAudioSource.Play();
-                }
-                else
-                {
-                    _previewAudioSource.Pause();
-                }
+                HandleMusic();
             }
-            if (GUILayout.Button("重置"))
+
+            //连续添加音符
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("间隔时间：", GUILayout.MaxWidth(60));
+            _intervalNoteTime = EditorGUILayout.FloatField(_intervalNoteTime, GUILayout.MaxWidth(50));
+            EditorGUILayout.LabelField("添加个数：", GUILayout.MaxWidth(60));
+            _createIntervalNoteCnt = EditorGUILayout.IntField(_createIntervalNoteCnt, GUILayout.MaxWidth(30));
+            EditorGUILayout.LabelField("上轨迹音符：", GUILayout.MaxWidth(70));
+            _createNoteUpType = EditorGUILayout.Toggle(_createNoteUpType);
+            if (GUILayout.Button("连续添加音符"))
             {
-                _isStartUpdate = false;
-                _nowMusicTime = 0;
-                _previewAudioSource.Stop();
+                AddIntervalNote();
             }
+            EditorGUILayout.EndHorizontal();
+
+            //重置按钮
+            if (GUILayout.Button("重置")) ResetMusic();
+
             //滑动条
             EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField("当前播放时间：",GUILayout.MaxWidth(80));
+            EditorGUILayout.LabelField("当前播放时间：", GUILayout.MaxWidth(80));
             _nowMusicTime = EditorGUILayout.Slider(_nowMusicTime, 0, _target.musicTime);
             EditorGUILayout.EndHorizontal();
-            EditorGUILayout.HelpBox("在音频播放时，D/F快速添加上轨迹音符，J/K快速添加下轨迹音符（时间小于音符到达打击点时间时无响应）", MessageType.Info);
-            // if (GUILayout.Button("添加音符"))
-            // {
-            //     _target.AddNote(_nowMusicTime);
-            // }
+            EditorGUILayout.HelpBox("在音频播放时，D/F快速添加上轨迹音符，J/K快速添加下轨迹音符（时间小于音符到达打击点时间时无响应），空格暂停", MessageType.Info);
+
+            //删除所有音符
             if (GUILayout.Button("删除所有音符"))
             {
                 _target.ClearNote();
             }
             base.OnInspectorGUI();
+        }
+
+        private void HandleMusic()
+        {
+            if (_isStartUpdate)
+            {
+                _previewAudioSource.clip = _target.audioClip;
+                _previewAudioSource.time = _nowMusicTime;
+                _previewAudioSource.pitch = _target.musicSpeed;
+                _previewAudioSource.Play();
+            }
+            else
+            {
+                _previewAudioSource.Pause();
+            }
+        }
+
+        private void ResetMusic()
+        {
+            _isStartUpdate = false;
+            _nowMusicTime = 0;
+            _previewAudioSource.Stop();
+        }
+
+        private void HandleKeyDown()
+        {
+            //在 EditorApplication.update ，Event会报空。这里用于快速添加音符
+            if (Event.current.type == EventType.KeyDown && _nowMusicTime >= _target.reachToBitPosTime)
+            {
+                if (Event.current.keyCode == KeyCode.J || Event.current.keyCode == KeyCode.K && _isStartUpdate)
+                {
+                    _target.AddNote(_nowMusicTime, ENotePos.Up, ENoteType.Click);
+                }
+                else if (Event.current.keyCode == KeyCode.D || Event.current.keyCode == KeyCode.F && _isStartUpdate)
+                {
+                    _target.AddNote(_nowMusicTime, ENotePos.Down, ENoteType.Click);
+                }
+                else if (Event.current.keyCode == KeyCode.Space)
+                {
+                    _isStartUpdate = !_isStartUpdate;
+                    if (_isStartUpdate)
+                        _previewAudioSource.Play();
+                    else
+                        _previewAudioSource.Pause();
+                }
+            }
+        }
+
+        private void AddIntervalNote()
+        {
+            if (_nowMusicTime >= _target.reachToBitPosTime)
+            {
+                for (int i = 0; i < _createIntervalNoteCnt; i++)
+                {
+                    _target.AddNote(_nowMusicTime + i * _intervalNoteTime, _createNoteUpType ? ENotePos.Up : ENotePos.Down, ENoteType.Click);
+                }
+            }
         }
     }
 }
